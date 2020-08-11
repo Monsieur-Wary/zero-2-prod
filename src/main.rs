31280b69d405a3833eb21c87ab168d151b367b41
Thibault Dessarps <dessarps.thibault@gmail.com>
2020-08-11 08:55:15 +0200

    :sparkles: health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
-fn main() {
-    println!("Hello, world!");
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000.");
+    zero_2_prod::run(listener)?.await
 }
