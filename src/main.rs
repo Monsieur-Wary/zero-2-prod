1c387bdc9c2ad0dfba9c2e1dc7d51bac671e7d82
Thibault Dessarps <dessarps.thibault@gmail.com>
2020-08-11 08:58:13 +0200

    :chart_with_upwards_trend: fmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000.");
+    let listener =
+        std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000.");
     zero_2_prod::run(listener)?.await
 }
